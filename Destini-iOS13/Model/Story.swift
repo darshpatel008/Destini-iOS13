5c3abbdb95fd50a01ed4082a1fec232330fcf4f6
darshviroja008 <darshviroja12345@gmail.com>
2024-11-06 23:00:44 +0530

    first-commit

---
+++ Destini-iOS13/Model/Story.swift
@@ -0,0 +1,9 @@
+struct Story
+{
+      var title : String
+      var choice1 : String
+      var choice1Destination : Int
+      var choice2 : String
+      var choice2Destination : Int
+        
+}
